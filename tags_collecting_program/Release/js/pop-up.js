1b25de23883a321a02267854868d54b28f84971f
OleksandrKara <oleksandr_kara@epam.com>
2013-12-18 16:38:17 +0200

    Looks like nice, need to improve gallery.

--- tags_collecting_program/Release/js/pop-up.js
+++ tags_collecting_program/Release/js/pop-up.js
@@ -1,39 +1,43 @@
+
 //basic fancybox setup
-	function validateEmail(email) { 
+	function validateName(name) { 
 		/*var reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		*/var reg = /^\D+$/;
-	    return reg.test(email);
+		*/
+		var reg = /^[A-zа-яА-ЯёЁа-яіїєґ ]+$/;
+	    return reg.test(name);
 	}
 
+	
+	
 	$(document).ready(function() {
 		$(".modalbox").fancybox();
 		$("#contact").submit(function() { return false; });
 
 		
 		$("#send").on("click", function(){
-			var emailval  = $("#email").val();
-			var msgval    = $("#msg").val();
-			var msglen    = msgval.length;
-			var mailvalid = validateEmail(emailval);
+			var nameval  = $("#name").val();
+			var phoneval = $("#user_phone").val();
+			var phonelen = phoneval.length;
+			var namevalid = validateName(nameval);
 			
-			if(mailvalid == false) {
-				$("#email").addClass("error");
+			if(namevalid == false) {
+				$("#name").addClass("error");
 			}
-			else if(mailvalid == true){
-				$("#email").removeClass("error");
-			}
-			
-			if(msglen < 4) {
-				$("#msg").addClass("error");
-			}
-			else if(msglen >= 4){
-				$("#msg").removeClass("error");
+			else if(namevalid == true){
+				$("#name").removeClass("error");
 			}
 			
-			if(mailvalid == true && msglen >= 4) {
+			if(phonelen < 10) {
+				$("#user_phone").addClass("error");
+			}
+			else if(phonelen >= 10){
+				$("#user_phone").removeClass("error");
+			}
+			
+			if(namevalid == true && phonelen >= 10) {
 				// if both validate we attempt to send the e-mail
 				// first we hide the submit btn so the user doesnt click twice
-				$("#send").replaceWith("<em>sending...</em>");
+				$("#send").replaceWith("<em>отправка...</em>");
 				
 				$.ajax({
 					type: 'POST',
